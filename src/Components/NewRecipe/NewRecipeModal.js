5bd1f2563d0acd5fdd3729ade34ab6820b1a57d7
Charles Buckley <charles.buckley@mobelux.com>
2020-08-25 14:23:51 -0400

    styling modal

--- src/Components/NewRecipe/NewRecipeModal.js
+++ src/Components/NewRecipe/NewRecipeModal.js
@@ -23,18 +23,24 @@ function getModalStyle() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
+    width: 300,
+    backgroundColor: '#e8e8e8',
     border: '2px solid #000',
     borderRadius: "10px",
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
   },
   formRow: {
     textAlign: 'center',
-    margin: '10px',
-    padding: '10px',
-  }
+    margin: '10px 0px',
+    padding: '10px 0px',
+    width: '100%',
+  },
+  buttonRow: {
+    backgroundColor: 'grey',
+    display: 'flex',
+    padding: '20px 0px',
+    justifyContent: 'space-around',
+  },
 }));
 
 function NewRecipeModal() {
@@ -47,11 +53,15 @@ function NewRecipeModal() {
     setOpen(true);
   };
 
+  const handleSubmit = () => {
+    alert('submit modal test');
+  };
+
   const handleClose = () => {
     setOpen(false);
   };
 
-  const body = (
+  const modalForm = (
     <div style={modalStyle} className={classes.paper}>
       <form id="form" className={classes.root} noValidate autoComplete="off">
         <div className={classes.formRow}>
@@ -81,24 +91,28 @@ function NewRecipeModal() {
               variant="contained"
               component="label"
           >
-          Upload Img
+            Image
             <input
               type="file"
               style={{ display: "none" }}
             />
           </Button>
         </div>
-        <div className={classes.formRow}> 
-          <Button variant="contained" color="primary">
-            Add
-          </Button>
-          <Button 
-          variant="contained" 
-          color="secondary"
-          onClick={handleClose}
-          >
-            Cancel
-          </Button>
+        <div> 
+          <div className={classes.buttonRow}>
+            <Button 
+            color="primary"
+            onClick={handleSubmit}
+            >
+              Add
+            </Button>
+            <Button 
+            color="secondary"
+            onClick={handleClose}
+            >
+              Cancel
+            </Button>
+          </div>
         </div>
       </form>
     </div>
@@ -114,7 +128,7 @@ function NewRecipeModal() {
         onClose={handleClose}
         aria-labelledby="form"
       >
-        {body}
+        {modalForm}
       </Modal>
     </div>
   );
